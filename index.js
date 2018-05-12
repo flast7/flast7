1c902f400ed689fe02ddb30d45806f3f3dd29dce
flast7 <39211462+flast7@users.noreply.github.com>
2018-05-12 10:29:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,47 +2,40 @@ const request = require('request');
 
 exports.getStats = (username, platform, callback) => {
 
-    var url = "https://fortnitetracker.com/profile/";
-    url += platform + "/" + encodeURIComponent(username);
+    var url = 'https://fortnitetracker.com/profile/';
+    url += `${platform}/${encodeURIComponent(username)}?old=1`
 
     request(url, (error, response, body) => {
-        // check platform
+
         if(platform != "pc" && platform != "psn" && platform != "xbl"){
             callback(new Error("Platform must be one of: 'pc', 'psn', 'xbl'"), null);
             return;
         }
 
-        // no response
         if(!response){
             callback(new Error("No response"), null);
             return;
         }
 
-        // player not found
         if(response.statusCode != 200){
             callback(new Error("Player not found"), null);
             return;
         }
 
-        // random error
         if(error){
             callback(new Error("Some error occured :("), null);
             return;
         }
 
-        // get text for last 7 days stats
         var last7 = body.substring(body.indexOf("var Last7") + 12);
         last7 = last7.substring(0, last7.indexOf("</script>") - 1);
 
-        // get text for account info
         var accountInfo = body.substring(body.indexOf("var accountInfo") + 18);
         accountInfo = accountInfo.substring(0, accountInfo.indexOf(";</script>"));
 
-        // parse to json objects
         var jsonStats = JSON.parse(last7);
         var jsonInfo = JSON.parse(accountInfo);
 
-        // obtain each value and put in dict
         var ret = {
             accountName: jsonInfo.Nickname,
             skinUrl: jsonInfo.EmblemUrl,
@@ -57,7 +50,6 @@ exports.getStats = (username, platform, callback) => {
             minutesPlayed: jsonStats[8].value
         }
 
-        // callback function with result
         callback(null, ret);
     });
 }
